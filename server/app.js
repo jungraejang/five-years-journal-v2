36ff4701ea6277e7d309618168c34cf902263e3d
jungraejang <jungrae.jang@gmail.com>
2022-10-13 18:08:34 -0400

    jwt token auth for server

--- server/app.js
+++ server/app.js
@@ -5,15 +5,14 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 var mongoose = require("mongoose");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 const bodyParser = require("body-parser");
 
+var indexRouter = require("./routes/index");
+
 var app = express();
-//hide this later in ENV
+//**********************************************hide this later in ENV****************************************************************
 const CONNECTION_URL =
-  "mongodb+srv://silvercoated:wjdfo0124@cluster0.rdau8vc.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://silvercoated:wjdfo0124@cluster0.rdau8vc.mongodb.net/five-years-journal-v2?retryWrites=true&w=majority";
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,27 +20,72 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:8080" }));
 // app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log(`Server is running on ${process.env.PORT || "3000"}`))
-  .catch((error) => console.log(error));
-
-// mongoose.set('useFindAndModify', false)
-
+//ROUTES************************************************************************
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
+const db = require("./models/index");
+const Role = db.role;
+
+db.mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`Server is running on ${process.env.PORT || "8080"}`);
+    initial();
+  })
+  .catch((error) => {
+    console.error("Connection error", error);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 // error handler
 app.use(function (err, req, res, next) {
